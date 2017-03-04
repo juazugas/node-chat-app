b3e725f8bbe7544cbccbcaeb197e1acf5319cf84
juazugas <juazuri@gmail.com>
2017-03-04 18:39:23 +0100

    Greet new user, and alert others.

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Chatbot',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Chatbot',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (data) => {
     console.log('createMessage:', data);
     io.emit('newMessage', {
@@ -22,6 +34,11 @@ io.on('connection', (socket) => {
       text: data.text,
       createdAt: new Date().getTime()
     });
+    /*socket.broadcast.emit('newMessage', {
+      from: data.from,
+      text: data.text,
+      createdAt: new Date().getTime()
+    });*/
   });
 
   socket.on('disconnect', (socket) => {
